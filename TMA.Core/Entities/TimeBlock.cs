960bf2f47f734ee1c5b830ac64bfd7d4ff000896
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-27 19:11:23 +0200

    Add implementation for TimeBlock

--- TMA.Core/Entities/TimeBlock.cs
+++ TMA.Core/Entities/TimeBlock.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TMA.Core.Entities
 {
-    internal class TimeBlock
+    public class TimeBlock
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public Guid Id { get; set; }
+
+        [Required]
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public TimeSpan Duration { get; set; }
+
+        [Required]
+        public Chore Chore { get; set; }
+     
     }
 }
