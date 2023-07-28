1e4f7ca09465bffd31603db57da6bf247fcc8318
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-28 14:02:52 +0200

    Add methods for IChoreService

--- TMA.Core/Interfaces/IChoreService.cs
+++ TMA.Core/Interfaces/IChoreService.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMA.SharedDtos.Dtos.Chore;
 
 namespace TMA.Core.Interfaces
 {
     public interface IChoreService
     {
+        Task<List<GetChoreDto>> GetAllChores();
+        Task<GetChoreDto> GetChoreById(Guid id);
+        Task<GetChoreDto> AddChore(AddChoreDto chore);
+        Task<GetChoreDto> UpdateChore(UpdateChoreDto chore);
+        Task<GetChoreDto> DeleteChore (Guid id);
     }
 }
