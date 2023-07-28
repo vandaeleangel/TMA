95fab1c18e2ea1a494a1db47ca22ee0ca043c46f
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-28 14:47:26 +0200

    Implement the Register()

---
+++ TMA.Core/Migrations/20230728122928_Init.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TMA.Core.Migrations
+{
+    /// <inheritdoc />
+    public partial class Init : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PasswordHash = table.Column<byte[]>(type: "varbinary(max)", nullable: false),
+                    PasswordSalt = table.Column<byte[]>(type: "varbinary(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Chores",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(50)", maxLength: 50, nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    UserId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chores", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Chores_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "TimeBlocks",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    StartTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    EndTime = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Duration = table.Column<TimeSpan>(type: "time", nullable: false),
+                    ChoreId = table.Column<Guid>(type: "uniqueidentifier", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeBlocks", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_TimeBlocks_Chores_ChoreId",
+                        column: x => x.ChoreId,
+                        principalTable: "Chores",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Chores_UserId",
+                table: "Chores",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_TimeBlocks_ChoreId",
+                table: "TimeBlocks",
+                column: "ChoreId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeBlocks");
+
+            migrationBuilder.DropTable(
+                name: "Chores");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
---
+++ TMA.Core/Services/AuthService.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMA.Core.Data;
+using TMA.Core.Entities;
+using TMA.Core.Interfaces;
+using TMA.SharedDtos.Dtos.User;
+
+namespace TMA.Core.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly DataContext _context;
+        private readonly IConfiguration _configuration;
+
+        public AuthService(DataContext context, IConfiguration configuration)
+        {
+            _context = context;
+            _configuration = configuration;
+        }
+
+      
+
+        public Task<string> Login(UserLoginDto user)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<RegistrationResult> Register(UserRegisterDto user)
+        {
+            if(await UserExists(user.Email))
+            {
+                return RegistrationResult.EmailAlreadyExists;
+            }
+
+            CreatePasswordHash(user.Password, out byte[] passwordHash, out byte[] passwordSalt);
+            User newUser = new User
+            {
+                Email = user.Email,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt,
+                Chores = new List<Chore>(),
+            };
+
+            _context.Users.Add(newUser);
+            await _context.SaveChangesAsync();
+
+            return RegistrationResult.Succes;
+        }
+
+        private void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<bool> UserExists(string email)
+        {
+            if(await _context.Users.AnyAsync(u => u.Email.ToLower() == email.ToLower()))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public enum RegistrationResult
+        {
+            Succes,
+            EmailAlreadyExists
+        }
+    }
+}
