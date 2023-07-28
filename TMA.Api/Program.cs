44d1e2fd9dc41c27b72f7760eaac9711eecb0f99
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-28 14:29:16 +0200

    Update the DataContext

--- TMA.Api/Program.cs
+++ TMA.Api/Program.cs
@@ -11,8 +11,7 @@ namespace TMA.Api
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
-            builder.Services.AddDbContext<DataContext>(options =>
-             options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+           builder.Services.AddDbContext<DataContext>();
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
--- TMA.Core/Data/DataContext.cs
+++ TMA.Core/Data/DataContext.cs
@@ -10,14 +10,20 @@ namespace TMA.Core.Data
 {
     public class DataContext :DbContext
     {
-        public DataContext(DbContextOptions<DataContext> options) :base(options)
-        {
-            
-        }
-
         public DbSet<User> Users { get; set; }
         public DbSet<Chore> Chores { get; set; }
         public DbSet<TimeBlock> TimeBlocks { get; set; }
+
+        public DataContext() { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=TMAdb;Trusted_Connection=True;TrustServerCertificate=true;");
+            optionsBuilder.EnableSensitiveDataLogging(true);
+        }
+
+
+
 
     }
 }
