e93e250ece956ae906d3d971fbd244115366a5dc
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-28 14:14:22 +0200

    Configure DataContext

---
+++ TMA.Core/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TMA.Core.Entities;
+
+namespace TMA.Core.Data
+{
+    public class DataContext :DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) :base(options)
+        {
+            
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Chore> Chores { get; set; }
+        public DbSet<TimeBlock> TimeBlocks { get; set; }
+
+    }
+}
