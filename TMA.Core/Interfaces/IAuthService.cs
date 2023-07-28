651ae07b7c16357fce8d8b1d59f99baebaf943f2
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-28 14:00:44 +0200

    Add methods for the IAuthService

--- TMA.Core/Interfaces/IAuthService.cs
+++ TMA.Core/Interfaces/IAuthService.cs
@@ -3,10 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMA.SharedDtos.Dtos.User;
 
 namespace TMA.Core.Interfaces
 {
     public interface IAuthService
     {
+        Task<Guid> Register(UserRegisterDto user);
+        Task<string> Login(UserLoginDto user);
+        Task<bool> UserExists(string email);
     }
 }
