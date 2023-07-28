f900059a2be1ef4b36d92d4d26736b3e9af43d93
vandaeleangel <angel.vandaele@student.howest.be>
2023-07-28 14:06:40 +0200

    Add methods for ITimeBlockService

--- TMA.Core/Interfaces/ITimeBlockService.cs
+++ TMA.Core/Interfaces/ITimeBlockService.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TMA.SharedDtos.Dtos.TimeBlock;
 
 namespace TMA.Core.Interfaces
 {
     public interface ITimeBlockService
     {
+        Task<List<GetTimeBlockDto>> GetAllTimeBlocks();
+        Task<GetTimeBlockDto> GetTimeBlockById(Guid id);
+        Task<GetTimeBlockDto> DeleteTimeBlock(Guid id);
+        Task<GetTimeBlockDto> AddTimeBlock(AddTimeBlockDto choreId);
+        Task<GetTimeBlockDto> UpdateEndTime(UpdateEndTimeDto choreId);
+
+        Task<GetTimeBlockDto> UpdateTimeBlock(UpdateTimeBlockDto timeBlock);
+
     }
 }
