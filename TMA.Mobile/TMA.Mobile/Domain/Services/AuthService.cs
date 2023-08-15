7da62d9183e200dae8b9941174a02e18cfc5a985
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-15 17:38:52 +0200

    Add an interface and class for the authenitcation

---
+++ TMA.Mobile/TMA.Mobile/Domain/Services/AuthService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TMA.Mobile.Domain.Dtos.User;
+
+namespace TMA.Mobile.Domain.Services
+{
+    public class AuthService : IAuthService
+    {
+        public Task<string> Login(UserLoginDto userLogin)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
