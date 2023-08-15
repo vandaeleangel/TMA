f09fb17b7342c784a33bff7588d6d8d1d9de412c
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-15 21:39:37 +0200

    Add the HttpClient + implementation for the class

---
+++ TMA.Mobile/TMA.Mobile/Domain/Services/Interfaces/IHttpConnector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TMA.Mobile.Domain.Services.Interfaces
+{
+    public interface IHttpConnector
+    {
+        Task<T> GetApiResult<T>(string uri);
+    }
+}
