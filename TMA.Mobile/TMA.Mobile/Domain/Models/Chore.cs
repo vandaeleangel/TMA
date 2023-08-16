74e9d2939fb685fec040f6f7c23f53f42852a01a
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-16 18:09:58 +0200

    Add the entities

---
+++ TMA.Mobile/TMA.Mobile/Domain/Models/Chore.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace TMA.Mobile.Domain.Models
+{
+    public class Chore
+    {
+        public Guid Id { get; set; }
+
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; } = string.Empty;
+        public TimeSpan Duration { get; set; }
+        public ICollection<TimeBlock> TimeBlocks { get; set; }
+        public User User { get; set; }
+    }
+}
