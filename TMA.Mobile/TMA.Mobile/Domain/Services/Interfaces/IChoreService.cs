8c917a47239ad4ded8200e9ddb3beafaf69e6d95
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-18 13:37:33 +0200

    Add interface related to Chores

---
+++ TMA.Mobile/TMA.Mobile/Domain/Services/Interfaces/IChoreService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TMA.Mobile.Domain.Dtos.Chore;
+using TMA.Mobile.Domain.Models;
+
+namespace TMA.Mobile.Domain.Services.Interfaces
+{
+    public interface IChoreService
+    {
+        Task<Chore> AddNewChore(AddChoreDto newChore);
+    }
+}
--- TMA.Mobile/TMA.Mobile/PageModels/NewChorePageModel.cs
+++ TMA.Mobile/TMA.Mobile/PageModels/NewChorePageModel.cs
@@ -2,10 +2,42 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using TMA.Mobile.Domain.Services.Interfaces;
+using Xamarin.Forms;
 
 namespace TMA.Mobile.PageModels
 {
     public class NewChorePageModel : FreshBasePageModel
     {
+        private IAppService _appService;
+        public string _name;
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                RaisePropertyChanged();
+            }
+        }
+
+        public Command SaveCommand { get; set; }
+        public Command CancelCommand { get; set; }
+        public NewChorePageModel(IAppService appService)
+        {
+            _appService = appService;
+            SaveCommand = new Command(Save);
+            CancelCommand = new Command(Cancel);
+        }
+
+        private async void Cancel(object obj)
+        {
+           await CoreMethods.PopPageModel(modal: true);
+        }
+
+        private void Save(object obj)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
