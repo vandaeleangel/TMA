e26a7fbe4491521828ddac506443d76978f14521
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-20 21:28:32 +0200

    Add method to fetch TimeBlocks related to certain date

---
+++ TMA.Mobile/TMA.Mobile/Domain/Services/Interfaces/ITimeBlockService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using TMA.Mobile.Domain.Dtos.TimeBlock;
+using TMA.Mobile.Domain.Models;
+
+namespace TMA.Mobile.Domain.Services.Interfaces
+{
+    public interface ITimeBlockService
+    {
+        Task<IEnumerable<TimeBlock>> GetFilteredTimeBlocks(TimeBlockQueryParametersDto param);
+    }
+}
