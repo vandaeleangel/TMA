2297fd8194e19abc482fa9e7a51c4d79189624d2
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-20 21:50:15 +0200

    Add TimeConvert, still issues with second DateTime

---
+++ TMA.Mobile/TMA.Mobile/Converters/DateTimeToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TMA.Mobile.Converters
+{
+    public class DateTimeToStringConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is DateTime dateTime)
+            {
+                return dateTime.ToString("yyyy-MM-dd"); 
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
