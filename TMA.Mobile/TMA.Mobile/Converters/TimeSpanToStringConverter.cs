61f0a86701d011cffc3e3a2d42a294b1acbf491e
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-21 11:06:33 +0200

    Add a button on the Detail HomeScreen

---
+++ TMA.Mobile/TMA.Mobile/Converters/TimeSpanToStringConverter.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TMA.Mobile.Converters
+{
+    public class TimeSpanToStringConverter :IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is TimeSpan timeSpan)
+            {
+                return timeSpan.ToString(@"hh\:mm"); // Format for displaying time span (hh:mm)
+            }
+
+            return string.Empty;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
