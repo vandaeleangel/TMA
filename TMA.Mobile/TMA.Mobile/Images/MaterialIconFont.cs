8b7136aef35d8427a780e1fc6ca64ae85a5da8a7
vandaeleangel <angel.vandaele@student.howest.be>
2023-08-25 12:04:37 +0200

    Add iconFont and imageconverter

---
+++ TMA.Mobile/TMA.Mobile/Images/MaterialIconFont.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TMA.Mobile.Images
+{
+    static class MaterialIconFont
+    {
+        public const string Stop = "\ue047";
+        public const string Warning = "\ue002";
+        public const string Av_timer = "\ue01b";
+        public const string Equalizer = "\ue01d";
+        public const string Hearing = "\ue023";
+        public const string Play_arrow = "\ue037";
+        public const string Mode_edit = "\ue254";
+        public const string Format_list_bulleted = "\ue241";
+        public const string Insert_chart_outlined = "\ue26a";
+        public const string Bar_chart = "\ue26b";
+    }
+}
